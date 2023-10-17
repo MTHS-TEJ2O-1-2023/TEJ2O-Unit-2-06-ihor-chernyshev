ca3ef368cbeb0cd6d7649e6cbe8130c5d7ce4672
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-17 16:37:31 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -4,15 +4,20 @@ Created on: Sep 2023
 This module turns an LED on a breadboard on and off.
 """
 
+# setup
 from microbit import *
 
 display.clear()
 display.show(Image.HAPPY)
 
 while True:
+
+    # light on
     if button_a.is_pressed():
         pin16.write_digital(1)
         display.show(Image.YES)
+
+    #light off
     if button_b.is_pressed():
         pin16.write_digital(0)
         display.show(Image.NO)
