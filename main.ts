c8579ef32900e0db2909499f68536a524ae7c2e4
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-17 12:37:20 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -5,15 +5,18 @@
  * This program turns an LED on a breadboard on and off.
 */
 
+// setup
 basic.clearScreen()
 pins.digitalWritePin(DigitalPin.P16, 0)
 basic.showIcon(IconNames.Happy)
 
+// light on
 input.onButtonPressed(Button.A, function () {
   basic.showIcon(IconNames.Yes)
   pins.digitalWritePin(DigitalPin.P16, 1)
 })
 
+// light off
 input.onButtonPressed(Button.B, function () {
   basic.showIcon(IconNames.No)
   pins.digitalWritePin(DigitalPin.P16, 0)
